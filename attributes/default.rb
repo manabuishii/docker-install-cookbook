e6c38c46c5b64c40ceeacd195b4355ce058d6360
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-02-17 15:53:23 +0900

    Change default docker version to 1.12.3

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 # docker version
 default[:docker_install][:docker][:distribution]="ubuntu-xenial"
-default[:docker_install][:docker][:version]='1.11.1'
+default[:docker_install][:docker][:version]='1.12.3'
 default[:docker_install][:docker][:setup_group]=true
 default[:docker_install][:docker][:members]='vagrant'
