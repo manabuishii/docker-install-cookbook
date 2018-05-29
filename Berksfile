4c2ded4de4406275b171249d39aab5371c1fbb90
Manabu ISHII <manabu.ishii.rb@gmail.com>
2018-05-29 16:23:45 +0900

    release 1.2.0

--- Berksfile
+++ Berksfile
@@ -1,5 +1,5 @@
 source "https://supermarket.chef.io/"
-cookbook 'apt', '= 2.7.0'
-cookbook 'docker', '= 2.16.0'
+cookbook 'apt', '= 7.0.0'
+cookbook 'docker', '= 4.2.0'
 cookbook 'localsetup', path: './site-cookbooks/localsetup'
 metadata
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'Manabu ISHII'
 license          'MIT'
 description      'Install docker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.0'
+version          '1.2.0'
 
-depends 'apt', '= 2.7.0'
-depends 'docker', '= 2.16.0'
+depends 'apt', '= 7.0.0'
+depends 'docker', '= 4.2.0'
